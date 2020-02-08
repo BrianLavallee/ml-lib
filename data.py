4c0f02d170319f3598bdfc21ddc75d352533ce61
Brian Lavallee <bplavallee@gmail.com>
2020-02-08 16:52:51 -0700

    modification for bank dataset

--- data.py
+++ data.py
@@ -17,3 +17,63 @@ def load_car():
             test_labels.append(item[-1])
 
     return train, train_labels, test, test_labels
+
+def convert_numeric(train, test, attr):
+    temp = [x[attr] for x in train]
+    temp.sort()
+    median = temp[int(len(temp)/2)]
+    for x in train:
+        x[attr] = 1 if x[attr] > median else 0
+
+    for x in test:
+        x[attr] = 1 if x[attr] > median else 0
+
+def convert_categorical(train, test, attr):
+    temp = [x[attr] for x in train]
+    counts = {}
+    majority = 0
+    label = -1
+    for x in temp:
+        if x == "unknown":
+            continue
+
+        if x not in counts:
+            counts[x] = 0
+
+        counts[x] += 1
+        if counts[x] > majority:
+            majority = counts[x]
+            label = x
+
+    for x in train:
+        if x[attr] == "unknown":
+            x[attr] = label
+
+    for x in test:
+        if x[attr] == "unknown":
+            x[attr] = label
+
+def load_bank():
+    train = []
+    train_labels = []
+    with open("./data/bank/train.csv", "r") as f:
+        for line in f:
+            item = line.strip().split(",")
+            train.append(item[:-1])
+            train_labels.append(item[-1])
+
+    test = []
+    test_labels = []
+    with open("./data/bank/test.csv", "r") as f:
+        for line in f:
+            item = line.strip().split(",")
+            test.append(item[:-1])
+            test_labels.append(item[-1])
+
+    for attr in [0, 5, 9, 11, 12, 13, 14]:
+        convert_numeric(train, test, attr)
+
+    for attr in [1, 3, 8, 15]:
+        convert_categorical(train, test, attr)
+
+    return train, train_labels, test, test_labels
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from data import load_car
+from data import load_car, load_bank
 from DecisionTree.split import entropy, majority_error, gini_index
 from DecisionTree.tree import DecisionTree
 
@@ -13,7 +13,7 @@ def error(dt, x, y):
 
     return count / len(x)
 
-def main():
+def car():
     train, train_labels, test, test_labels = load_car()
 
     for i in range(1, 7):
@@ -22,5 +22,19 @@ def main():
         print("train error: {}".format(error(dt, train, train_labels)))
         print("test error: {}".format(error(dt, test, test_labels)))
         print()
+
+def bank():
+    train, train_labels, test, test_labels = load_bank()
+
+    for i in range(1, 17):
+        print("maximum depth: {}".format(i))
+        dt = DecisionTree(train, train_labels, [i for i in range(len(train[0]))], majority_error, i)
+        print("train error: {}".format(error(dt, train, train_labels)))
+        print("test error: {}".format(error(dt, test, test_labels)))
+        print()
+
+def main():
+    # car()
+    bank()
 
 main()
